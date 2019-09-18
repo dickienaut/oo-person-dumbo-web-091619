cfad9e50864d7487148af63da4d094f9c2c1804b
lyle dickie <lyle.e.dickie@gmail.com>
2019-09-18 18:45:21 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,11 +1,11 @@
 class Person
-  attr_reader :name, :hygeine_points, :happiness
+  attr_reader :name, :hygeine, :happiness
   attr_accessor :bank_account
   
   def initialize(name)
     @name = name
     @bank_account = 25
-    @hygeine_points = 8
+    @hygeine = 8
     @happiness = 8
   end
     
